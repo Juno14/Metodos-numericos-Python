d323edbf0e054fa19d48f05e6c9985b9464f45ee
Juno14 <52438259+Juno14@users.noreply.github.com>
2019-08-14 18:47:42 -0500

    Update Lineal.py

--- Lineal.py
+++ Lineal.py
@@ -62,3 +62,48 @@ def gauss(a, b):
           s = s + c[i, j]*x
           x = c[i, n+1] - s
      print(s)       
+#Jacobi
+def jacobi(a, b, x):
+    n = len(x)
+    t = x.copy()
+    for i in range(n):
+        s = 0
+        for j in range(n):
+            if i != j:
+                s = s + a[i, j] * t[j]
+        x[i] = (b[i] - s) / a[i, i]
+    return x
+#Jacobi mejorado
+def jacobim(a, b, x, e, m):
+    n = len(x)
+    t = x.copy()
+    for k in range(m):
+        x = jacobi(a, b, x)
+        d = np.linalg.norm(np.array(x) - np.array(t))
+        if d < e:
+            return [x, k]
+        else:
+            t = x. copy()
+    return [[], m]
+#Gauss-Seidel
+def gaussseidel(a, b, x):
+    n = len(x)
+    for i in range(n):
+        s = 0
+        for j in range(n):
+            if i != j:
+                s = s + a[i, j] * x[j]
+        x[i] = (b[i] - s) / a[i, i]
+    return x
+#Gauss-Seidel mejorado
+def gsm(a, b, x, e, m):
+    n = len(x)
+    t = x.copy()
+    for k in range(m):
+        x = gaussseidel(a, b, x)
+        d = np. linalg.norm(array(x) - array(t), inf)
+        if d < e:
+            return [x, k]
+        else:
+            t = x.copy()
+    return [[], m]
