da0048e19e15d40980faed4a0653f418525e03ff
Juno14 <52438259+Juno14@users.noreply.github.com>
2019-08-14 19:35:23 -0500

    Update Lineal.py

--- Lineal.py
+++ Lineal.py
@@ -61,7 +61,31 @@ def gauss(a, b):
      for j in range (i, n):
           s = s + c[i, j]*x
           x = c[i, n+1] - s
-     print(s)       
+     print(s)
+#Gauss con pivote
+def gausspiv(a, b):
+    n = len(b)
+    c = np.concatenate([a, b], axis = 1)
+    for e in range (n):
+        p = e
+        for i in range (e+1, n):
+            if abs(c[i, e]) > abs([p, e]):
+                p = i
+        for j in range (e+1, n):
+            t = c[e, j]
+            c[e, j] = c[p, j]
+            c[p, j] = t
+        t = c[e, e]
+        if abs(t) < 1e - 20:
+            return []
+        c[e, e:] = c[e, e:] / c[e, e]
+        for i in range (e+1, n):
+            c[i, e:] = c[i, e:] - c[i, e] * c[e, e:]
+    x = zeros([n])
+    x[n-1] = c[n-1, n]
+    for i in range (n-2, -1, -1):
+        x[i] = c[i, n] - np.dot(x[i+1:n], c[i, i+1:n])
+    return x
 #Jacobi
 def jacobi(a, b, x):
     n = len(x)
